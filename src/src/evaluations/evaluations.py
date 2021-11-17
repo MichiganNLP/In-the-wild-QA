eeb767988b86fd1e6f8c5ba9c84a032fd46e96de
Neo <dnaihao@umich.edu>
2021-11-17 12:25:58 -0500

    Added Andrew's IOU code to our evaluation

--- src/src/evaluations/evaluations.py
+++ src/src/evaluations/evaluations.py
@@ -2,11 +2,18 @@ from nltk.translate.bleu_score import sentence_bleu
 from nltk import word_tokenize
 from rouge import Rouge
 import warnings
+import numpy as np
+from tqdm import tqdm
+from collections import defaultdict
+
 
 warnings.filterwarnings("ignore")   # filter user warning for BLEU when overlap is 0
 
 
 def evaluate(model_name, preds, test_data):
+    """ 
+    Evaluation for QA
+    """
     sources = [itm['source'] for itm in test_data]
     labels = [[itm['target']] for itm in test_data]
 
@@ -22,7 +29,17 @@ def evaluate(model_name, preds, test_data):
     print(f"ROUGE 3: {round(evl.ROUGE(3) * 100, 2)}%")
 
 
+def evidence_evaluation(model_name: str, preds: list, test_data: list):
+    """ 
+    Evaluation for evidence finding
+    """
+
+    gt_spans = [[[start, end] for span in itm["evidences"] for _, [start, end] in span] for itm in test_data]
+    evl = Evidence_Evaluation(preds, gt_spans)    
+
+
 class Evaluation():
+    """ Evaluation class for QA """
 
     def __init__(self, sources: list, preds: list, labels: list):
         
@@ -80,10 +97,109 @@ class Evaluation():
         return tt_rouge / len(self.preds)
 
 
+class Evidence_Evaluation():
+    """ Evaluation for evidence finding """
+
+    def __init__(self, preds: list, labels: list):
+        self.preds = preds 
+        self.labels = labels
+
+    def _f1(self, _p, _r):
+        if _p == 0 or _r == 0:
+            return 0
+        return 2 * _p * _r / (_p + _r)
+
+    def _calculate_iou(self, span_1, span_2):
+        num = len(
+                set(range(round(span_1[0]), round(span_1[1])))
+                & set(range(round(span_2[0]), round(span_2[1])))
+        )
+        denom = len(
+            set(range(round(span_1[0]), round(span_1[1])))
+            | set(range(round(span_2[0]), round(span_2[1])))
+        )
+        iou = 0 if denom == 0 else num / denom
+        return iou
+
+    def iou_f1(self, threshold=0.5, pred_threshold=1):
+        """
+        IOU-F1
+        
+        Code originally implemented for paper Micromodels for Efficient, Explainable, and Reusable Systems:
+            A Case Study on Mental Health by Lee et al.
+        Reference: https://github.com/MichiganNLP/micromodels/blob/empathy/empathy/run_experiments.py#L292-L333
+        
+        threshold: take into account the prediction when its iou with gold span is larger than
+            this threshold.
+        pred_threshold: if two predictions' iou smaller than it, will only consider the predic-
+            tion span with the larger value. '1' means that all prediction spans will be consi-
+            dered.
+        """
+        predictions = self.preds
+        gold = self.labels
+        assert len(predictions) == len(gold)
+        all_f1_vals = []
+
+        for idx, pred_spans in tqdm(enumerate(predictions)):
+            gold_spans = gold[idx]
+
+            ious = defaultdict(float)
+
+            # repeated predictions
+            overlapped = list()
+
+            for i in range(len(pred_spans)):
+                ps_1 = pred_spans[i]
+                for j in range(i + 1, len(pred_spans)):
+                    ps_2 = pred_spans[j]
+                    iou = self._calculate_iou(ps_1, ps_2)
+                    if iou > pred_threshold:
+                        overlapped.append((i, j))
+
+
+            for i, pred_span in enumerate(pred_spans):
+                best_iou = 0.0
+                for gold_span in gold_spans:
+
+                    iou = self._calculate_iou(pred_span, gold_span)
+
+                    if iou > best_iou:
+                        best_iou = iou
+                ious[i] = best_iou
+            
+            # delete overlapped predictions
+            for (i, j) in overlapped:
+                assert i in ious and j in ious
+                if ious[i] >= ious[j]:
+                    del ious[j]
+                else:
+                    del ious[i]
+
+            threshold_tps = sum(int(x >= threshold) for x in ious.values())
+
+            micro_r = threshold_tps / len(gold_spans) if len(gold_spans) > 0 else 0
+            micro_p = threshold_tps / len(pred_spans) if len(pred_spans) > 0 else 0
+            micro_f1 = self._f1(micro_r, micro_p)
+            if len(pred_spans) == 0 and len(gold_spans) == 0:
+                all_f1_vals.append(1)
+            else:
+                all_f1_vals.append(micro_f1)
+
+        return np.mean(all_f1_vals)
+
+
+
+###########################################################
+###########################################################
+# testing for the evaluation class
+
 
 TEST_SOURCE = ["he began by starting"]
 TEST_PREDS = ["he began by starting"]
 TEST_LABELS = [["he began by asd", "he began asd ads"]]
+
+TEST_EVIDENCE_PREDS = [[[1.2, 3.1], [4.5, 6.7]]]
+TEST_EVIDENCE_LABELS = [[[1.2, 3.5], [2.3, 5.0]]]
 
 
 if __name__ == "__main__":
@@ -91,6 +207,9 @@ if __name__ == "__main__":
     print(evl.exact_match())
     print(evl.BLEU(2))
     print(evl.ROUGE(2))
+
+    ev_evl = Evidence_Evaluation(TEST_EVIDENCE_PREDS, TEST_EVIDENCE_LABELS)
+    print(ev_evl.iou_f1())
     
 
 

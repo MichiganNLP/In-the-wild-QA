12f3c26699adfe4876e0cd375a6e65f894ac6a70
Santiago Castro <sacastro@umich.edu>
2022-04-22 11:35:21 -0400

    Improve the video frame files scanning time and print progress

--- src/clip_decoder_source_training.sh
+++ src/clip_decoder_source_training.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-output_ckpt_dir=ckpts/clip_decoder/text_visual_source_trained/
+output_ckpt_dir=ckpts/clip_decoder/text_visual_source_trained
 num_train_epochs=100
 train_batch_size=8
 eval_batch_size=16
-log_dir=logs/clip_decoder/
+log_dir=logs/clip_decoder
 log_path=logs/clip_decoder/source_training_parallel_baseline.log
 max_seq_length=16
 max_vid_length=2048
@@ -15,13 +15,11 @@ mkdir -p ${output_ckpt_dir}
 mkdir -p ${log_dir}
 
 python -m src clip_decoder \
-    --train_data_path example_data/tvqa-data/train.json \
-    --dev_data_path example_data/tvqa-data/val.json \
-    --test_data_path example_data/tvqa-data/val.json \
-    --frames_path video_features/tvqa_frames_hq \
-    --is_tvqa \
-    --n_gpu 4 \
-    --accelerator ddp \
+    --train_data_path /scratch/mihalcea_root/mihalcea0/shared_data/tvqa_private_share/tvqa_qa_release/train.json \
+    --dev_data_path /scratch/mihalcea_root/mihalcea0/shared_data/tvqa_private_share/tvqa_qa_release/val.json \
+    --test_data_path /scratch/mihalcea_root/mihalcea0/shared_data/tvqa_private_share/tvqa_qa_release/test.json \
+    --frames_path /scratch/mihalcea_root/mihalcea0/shared_data/tvqa_private_share/previously_extracted/extracted_fns/frames_hq \
+    --strategy ddp_find_unused_parameters_false \
     --output_ckpt_dir ${output_ckpt_dir} \
     --num_train_epochs ${num_train_epochs} \
     --train_batch_size ${train_batch_size} \
